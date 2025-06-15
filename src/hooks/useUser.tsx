de8fb4bd664383b82d063c5d4ba07bf969585c4f
Giovanni Tonussi <gtonussi@gmail.com>
2025-06-15 17:32:53 -0300

    fix: Deletes the .env file

--- src/hooks/useUser.tsx
+++ src/hooks/useUser.tsx
@@ -4,14 +4,15 @@ import { useQuery } from "@tanstack/react-query";
 
 import { UserResponse } from "@/types/user-response";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_URL = "https://reqres.in";
+const USERS_ENDPOINT = "/api/users?page=";
 
 export async function getUsers(): Promise<UserResponse> {
-  if (!API_URL) throw new Error("API_URL is not defined");
+  const response = await fetch(`${API_URL + USERS_ENDPOINT}1`);
 
-  const response = await fetch(`${API_URL}/users`);
-
-  if (!response.ok) throw new Error("Failed to fetch users");
+  if (!response.ok) {
+    throw new Error("Failed to fetch users");
+  }
 
   return response.json();
 }
