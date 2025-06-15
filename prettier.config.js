17b2e0c6e2ff83f46ef57f4422f525f5ff9a2956
Giovanni Tonussi <gtonussi@gmail.com>
2025-06-15 13:37:33 -0300

    feat: Implements dark mode

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  trailingComma: "all",
+  bracketSpacing: true,
+  arrowParens: "always",
+  endOfLine: "lf",
+  plugins: ["prettier-plugin-tailwindcss"],
+};
