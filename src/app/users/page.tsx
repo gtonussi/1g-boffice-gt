9f6989c1de6edd4aaf512287ec443cdf267c6efc
Giovanni Tonussi <gtonussi@gmail.com>
2025-06-16 10:18:08 -0300

    fix: Wrapers UsersTable in a Suspense component

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,7 +1,12 @@
 "use client";
+import { Suspense } from "react";
 
 import { UsersTable } from "@/components/ui/users-table";
 
 export default function Home() {
-  return <UsersTable />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <UsersTable />
+    </Suspense>
+  );
 }
